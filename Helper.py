946ca4f7f2cfb29316acc457979c068624aea155
Swastik-Swarup-Dash <143310346+Swastik-Swarup-Dash@users.noreply.github.com>
2024-06-17 16:23:40 +0530

    updated text_functions

--- Helper.py
+++ Helper.py
@@ -338,3 +338,40 @@ def predict_on_sentence(model, sentence):
   pred_label = tf.squeeze(tf.round(pred_prob)).numpy()
   print(f"Pred: {pred_label}", "(real disaster)" if pred_label > 0 else "(not real disaster)", f"Prob: {pred_prob[0][0]}")
   print(f"Text:\n{sentence}")
+
+
+
+
+def get_lines(filename):
+  with open(filename, "r") as f:
+    return f.readlines()
+
+
+
+def preprocess_text_with_line_numbers(filename):
+  input_lines = get_lines(filename) 
+  abstract_lines = "" 
+  abstract_samples = [] 
+  
+  # Loop through each line in target file
+  for line in input_lines:
+    if line.startswith("###"): # check to see if line is an ID line
+      abstract_id = line
+      abstract_lines = "" # reset abstract string
+    elif line.isspace(): # check to see if line is a new line
+      abstract_line_split = abstract_lines.splitlines() # split abstract into separate lines
+
+      # Iterate through each line in abstract and count them at the same time
+      for abstract_line_number, abstract_line in enumerate(abstract_line_split):
+        line_data = {} # create empty dict to store data from line
+        target_text_split = abstract_line.split("\t") # split target label from text
+        line_data["target"] = target_text_split[0] # get target label
+        line_data["text"] = target_text_split[1].lower() # get target text and lower it
+        line_data["line_number"] = abstract_line_number # what number line does the line appear in the abstract?
+        line_data["total_lines"] = len(abstract_line_split) - 1 # how many total lines are in the abstract? (start from 0)
+        abstract_samples.append(line_data) # add line data to abstract samples list
+    
+    else: # if the above conditions aren't fulfilled, the line contains a labelled sentence
+      abstract_lines += line
+  
+  return abstract_samples       
